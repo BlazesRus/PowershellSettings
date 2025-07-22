2477af31b44ad75c7b8d18679f3ce2fd46d72032
BlazesRus (James) <BlazesRus@yahoo.com>
2025-07-22 00:52:47 -0400

    Updated file to include MIT-0 text

--- PowerShellTerminal.ps1
+++ PowerShellTerminal.ps1
@@ -27,6 +27,16 @@
     "terminal.integrated.defaultProfile.windows": "PowerShell 7",
     "terminal.integrated.automationProfile.windows": "PowerShell 7",
  - Customize ProfileSettings.psd1 for custom variables
+ 
+File under
+MIT No Attribution License (MIT-0)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #>
 
 if($env:USERPROFILE){
